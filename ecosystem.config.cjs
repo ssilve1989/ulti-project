92db36a5e2cdddc4bbb6a6f02eae2997cba93c41
Steven Silvestri <steve@trumidtech.com>
2023-12-08 09:21:26 -0700

    chore: remove older identifiers

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,12 +5,6 @@ module.exports = {
       script: 'dist/main.js',
       FORCE_COLOR: 1,
       name: 'ulti-project-bot',
-      env: {
-        CLIENT_ID: '1134625038449000551',
-        GUILD_ID: '913492538516717578',
-        PUBLIC_KEY:
-          'd1c58a67436ed1b8d0dd184d97d6b0140aabc49d526f614623b6a82418b1b4e0',
-      },
     },
   ],
 };
