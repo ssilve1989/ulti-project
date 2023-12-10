8f99e37ec442f075672f02b450cb12a0f6b5f8ad
Steven Silvestri <steve@trumidtech.com>
2023-12-10 09:11:11 -0700

    refactor(signups): add approval status to signup documents

--- src/signups/signup.service.ts
+++ src/signups/signup.service.ts
@@ -1,22 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { InjectFirestore } from '../firebase/firebase.decorators.js';
-import { Firestore } from 'firebase-admin/firestore';
-import { Signup } from './signup.interfaces.js';
+import { CollectionReference, Firestore } from 'firebase-admin/firestore';
+import { Signup, SignupRequest } from './signup.interfaces.js';
 
 @Injectable()
 class SignupService {
-  constructor(@InjectFirestore() private readonly firestore: Firestore) {}
-
-  public upsertSignup(signup: Signup) {
-    const key = this.getKeyForSignup(signup);
-
-    return this.firestore
-      .collection('signups')
-      .doc(key)
-      .set(signup, { merge: true });
+  private readonly collection: CollectionReference<Signup>;
+  constructor(@InjectFirestore() private readonly firestore: Firestore) {
+    this.collection = this.firestore.collection(
+      'signups',
+    ) as CollectionReference<Signup>;
   }
 
-  private getKeyForSignup({ username, encounter }: Signup) {
+  public async upsertSignup(signup: SignupRequest) {
+    const key = this.getKeyForSignup(signup);
+
+    const document = this.collection.doc(key);
+    const snapshot = await document.get();
+
+    if (snapshot.exists) {
+      return document.set(signup, {
+        // only update these fields if the document already exists. This allows approvals that were made to remain intact
+        mergeFields: ['fflogsLink', 'character', 'world', 'availability'],
+      });
+    }
+
+    return document.create({ ...signup, approved: false });
+  }
+
+  private getKeyForSignup({ username, encounter }: SignupRequest) {
     // usernames are supposed to be unique right? The recent change discord
     // made to remove #discriminator from the username might make this wonky
     return `${username}-${encounter}`;
