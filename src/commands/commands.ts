3b93e6b10b1ba4ff680d7796849e563348129ebc
Steven Silvestri <steve@trumidtech.com>
2023-12-12 11:26:31 -0700

    feat(status-command): adds `/status` as a slash command

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,3 +1,4 @@
 import { SignupCommandData } from '../signups/signup.consts.js';
+import { StatusSlashCommand } from '../status/commands/status-slash.command.js';
 
-export const COMMANDS = [SignupCommandData];
+export const COMMANDS = [SignupCommandData, StatusSlashCommand];
