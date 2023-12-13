75973689e52d761288124f9a1b07f315a22de92a
Steven Silvestri <steven.m.silvestri@gmail.com>
2023-12-13 13:04:49 -0700

    refactor: remove unnecessary interactions module, misc cleanup

--- src/commands/slash-commands.ts
+++ src/commands/slash-commands.ts
@@ -1,4 +1,4 @@
-import { SignupCommandData } from './signup-slash-command.js';
+import { SignupSlashCommand } from './signup-slash-command.js';
 import { StatusSlashCommand } from './status-slash-command.js';
 
-export const SLASH_COMMANDS = [SignupCommandData, StatusSlashCommand];
+export const SLASH_COMMANDS = [SignupSlashCommand, StatusSlashCommand];
