eaeac8cc457115a5635ecbae4c7133c768460bed
Steven Silvestri <steven.m.silvestri@gmail.com>
2024-02-02 12:47:56 -0700

    chore: remove unused dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module.js';
-import { ConfigService } from '@nestjs/config';
-import { AppConfig } from './app.config.js';
 import { Logger } from 'nestjs-pino';
+import { AppModule } from './app.module.js';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.createApplicationContext(AppModule, {
     bufferLogs: true,
-    cors: true,
   });
 
   app.useLogger(app.get(Logger));
-
-  const configService = app.get<ConfigService<AppConfig, true>>(ConfigService);
-
   app.enableShutdownHooks();
-
-  await app.listen(configService.get('PORT'));
 }
 
 bootstrap();
