14fe69c1ba25f30d48e09490e8b7f3bac42a9e0c
Steve <steven.m.silvestri@gmail.com>
2024-02-16 11:36:16 -0700

    refactor(signups): add followup messages on approval/decline (#92)
    Sends a public approval ping if the signup is approved
    Sends a private DM message if the signup is denied

--- src/signups/signup.consts.ts
+++ src/signups/signup.consts.ts
@@ -1,14 +1,24 @@
 export const SIGNUP_MESSAGES = {
   CONFIRMATION_TIMEOUT:
     'Confirmation not received within 1 minute, cancelling signup. Please use /signup if you wish to try again.',
-  SIGNUP_SUBMISSION_CANCELLED:
-    'Signup canceled. Please use /signup if you wish to try again.',
-  SIGNUP_SUBMISSION_CONFIRMED:
-    'Confirmed! A coordinator will review your submission and reach out to you soon. You can use `/status` to review the state of your signups.',
   MISSING_SIGNUP_REVIEW_CHANNEL:
     'A review channel has not been configured for this bot. Please contact an administrator',
   MISSING_SETTINGS:
     'No settings have been configured for this bot. Commands may not function until properly configured',
+  PROG_DM_TIMEOUT:
+    "You didn't respond in time. Please manually update the google sheet with the intended prog point",
+  SIGNUP_SUBMISSION_CANCELLED:
+    'Signup canceled. Please use /signup if you wish to try again.',
+  SIGNUP_SUBMISSION_CONFIRMED:
+    'Confirmed! A coordinator will review your submission and reach out to you soon. You can use `/status` to review the state of your signups.',
+  SIGNUP_SUBMISSION_DENIED: `
+Your signup has been denied by a coordinator. Please ensure you have met the following requirements for signing up for your encounter
+- You submitted proof of the correct prog point for the encounter
+- You have not cleared this encounter before
+You can reach out to a coordinator to discuss any issues
+`,
+  UNEXPECTED_PROG_SELECTION_ERROR:
+    'Sorry an unexpected error has occurred. Please report this problem and manually update the google sheet with the intended prog point',
 };
 
 export const SIGNUP_REVIEW_REACTIONS: Record<
