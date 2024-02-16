697e6f3ae6d01cfa99aa7957bc58a93d2608ec90
Steven Silvestri <steven.m.silvestri@gmail.com>
2024-02-16 08:02:44 -0700

    refactor: setup signup channel for public postings

--- src/settings/settings.interfaces.ts
+++ src/settings/settings.interfaces.ts
@@ -1,7 +1,8 @@
 import { DocumentData } from 'firebase-admin/firestore';
 
 export interface Settings extends DocumentData {
-  reviewerRole?: string;
   reviewChannel: string;
+  reviewerRole?: string;
+  signupChannel?: string;
   spreadsheetId?: string;
 }
