f9873869371a0903e20fa9ae042c27d5b6b7a220
Steve <steven.m.silvestri@gmail.com>
2024-03-10 10:29:13 -0600

    refactor: integrate sentry error handling (#135)

---
+++ src/sentry/sentry.module.ts
@@ -0,0 +1,27 @@
+import { Module, OnApplicationShutdown, OnModuleInit } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
+import { AppConfig } from '../app.config.js';
+
+@Module({
+  imports: [ConfigModule],
+})
+export class SentryModule implements OnModuleInit, OnApplicationShutdown {
+  constructor(private readonly configService: ConfigService<AppConfig>) {}
+
+  onModuleInit() {
+    Sentry.init({
+      integrations: [nodeProfilingIntegration()],
+      // Performance Monitoring
+      tracesSampleRate: 1.0, //  Capture 100% of the transactions
+      // Set sampling rate for profiling - this is relative to tracesSampleRate
+      profilesSampleRate: 1.0,
+      environment: this.configService.get('NODE_ENV'),
+    });
+  }
+
+  async onApplicationShutdown() {
+    await Sentry.close(2000);
+  }
+}
