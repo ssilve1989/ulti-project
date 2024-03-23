d058438cc72f0b9df72d996b88c13f81503c0608
Steve <steven.m.silvestri@gmail.com>
2024-03-23 17:40:18 -0600

    fix(signups): assert casing of character is Capital Case (#179)

---
+++ src/common/decorators/to-casing.ts
@@ -0,0 +1,12 @@
+import { Transform } from 'class-transformer';
+
+type CaseTransformer = (value: string) => string;
+
+/**
+ * Decorator to transform a string to a specified casing. For use in conjunction
+ * with change-case and class-transformer
+ * @param fn
+ * @returns
+ */
+export const ToCasing = (fn: CaseTransformer) =>
+  Transform(({ value }) => fn(value));
