3eb26ad881a31c1631399512d16cc051ab4324b8
Steven Silvestri <steven.m.silvestri@gmail.com>
2024-03-23 14:17:40 -0600

    refactor(sheets): enable http2

--- src/sheets/sheets.config.ts
+++ src/sheets/sheets.config.ts
@@ -2,15 +2,17 @@ import { registerAs } from '@nestjs/config';
 import Joi from 'joi';
 
 export interface SheetsConfig {
+  GOOGLE_APIS_HTTP2: boolean;
   GOOGLE_UNIVERSE_DOMAIN: string;
-  SHEET_PROG_NAME: string;
   SHEET_EARLY_PROG_NAME: string;
+  SHEET_PROG_NAME: string;
 }
 
 const schema = Joi.object({
+  GOOGLE_APIS_HTTP2: Joi.boolean().default(true),
   GOOGLE_UNIVERSE_DOMAIN: Joi.string().default('googleapis.com'),
-  SHEET_PROG_NAME: Joi.string().default('MID/LATE PROG (Season 4)'),
   SHEET_EARLY_PROG_NAME: Joi.string().default('EARLY PROG (Season 4)'),
+  SHEET_PROG_NAME: Joi.string().default('MID/LATE PROG (Season 4)'),
 });
 
 export const sheetsConfig = registerAs<SheetsConfig>('sheets', () => {
