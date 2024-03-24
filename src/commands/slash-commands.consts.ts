fc6df0fe0f6e86117bebab350f7349252647588a
Steve <steven.m.silvestri@gmail.com>
2024-03-24 11:30:09 -0600

    refactor: order encounter choices by release date (#181)

--- src/commands/slash-commands.consts.ts
+++ src/commands/slash-commands.consts.ts
@@ -1,12 +1,12 @@
 import { Encounter } from '../encounters/encounters.consts.js';
 
 export const ENCOUNTER_CHOICES = [
-  { name: 'The Epic of Alexander (Ultimate)', value: Encounter.TEA },
   { name: 'The Omega Protocol (Ultimate)', value: Encounter.TOP },
+  { name: 'Dragonsong Reprise (Ultimate)', value: Encounter.DSR },
+  { name: 'The Epic of Alexander (Ultimate)', value: Encounter.TEA },
+  { name: 'The Weapons Refrain (Ultimate)', value: Encounter.UWU },
   {
     name: 'The Unending Coil of Bahamut (Ultimate)',
     value: Encounter.UCOB,
   },
-  { name: 'The Weapons Refrain (Ultimate)', value: Encounter.UWU },
-  { name: 'Dragonsong Reprise (Ultimate)', value: Encounter.DSR },
 ];
