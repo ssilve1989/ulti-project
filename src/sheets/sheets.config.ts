6072332856ffbe655122f0e0efc76009b9e23251
Steven Silvestri <steven.m.silvestri@gmail.com>
2024-03-31 11:23:35 -0600

    fix: disable googleapis http2 by default

--- src/sheets/sheets.config.ts
+++ src/sheets/sheets.config.ts
@@ -9,7 +9,10 @@ export interface SheetsConfig {
 }
 
 const schema = Joi.object({
-  GOOGLE_APIS_HTTP2: Joi.boolean().default(true),
+  // https://github.com/googleapis/google-api-nodejs-client/issues/3187
+  // http2 + timeout maybe doesn't work well? We seem to be getting a lot more hanging requests
+  // since this was turned on.
+  GOOGLE_APIS_HTTP2: Joi.boolean().default(false),
   GOOGLE_UNIVERSE_DOMAIN: Joi.string().default('googleapis.com'),
   SHEET_EARLY_PROG_NAME: Joi.string().default('EARLY PROG (Season 4)'),
   SHEET_PROG_NAME: Joi.string().default('MID/LATE PROG (Season 4)'),
