7815565b91027e94b52908b1b069950cf181824d
Steve <steven.m.silvestri@gmail.com>
2024-03-31 08:07:27 -0600

    fix: make casing consistent (#190)
    special formatting for display purposes should be left to the google
    sheet ops not any other place. Every other place such as
    - firestore
    - command queries
    should be consistently lowercase. 
    
    #179 introduced a regression since lookup commands were trying to match
    on lowercase but data was being stored as capital case

--- src/commands/lookup/lookup.dto.ts
+++ src/commands/lookup/lookup.dto.ts
@@ -1,16 +1,17 @@
+import { capitalCase } from 'change-case';
 import { IsOptional, IsString } from 'class-validator';
-import { ToLowercase } from '../../common/decorators/to-lowercase.js';
+import { ToCasing } from '../../common/decorators/to-casing.js';
 import { SignupDocument } from '../../firebase/models/signup.model.js';
 
 export class LookupInteractionDto
   implements Pick<SignupDocument, 'character' | 'world'>
 {
   @IsString()
-  @ToLowercase()
+  @ToCasing(capitalCase)
   character: string;
 
   @IsString()
   @IsOptional()
-  @ToLowercase()
+  @ToCasing(capitalCase)
   world: string;
 }
