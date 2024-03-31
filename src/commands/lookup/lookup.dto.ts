c84557a81574949d7b40cafefbfbca17bec1979e
Steve <steven.m.silvestri@gmail.com>
2024-03-31 08:08:47 -0600

    Revert "fix: make casing consistent" (#192)
    Reverts ssilve1989/ulti-project#190
    
    some commits were missed in that PR

--- src/commands/lookup/lookup.dto.ts
+++ src/commands/lookup/lookup.dto.ts
@@ -1,17 +1,16 @@
-import { capitalCase } from 'change-case';
 import { IsOptional, IsString } from 'class-validator';
-import { ToCasing } from '../../common/decorators/to-casing.js';
+import { ToLowercase } from '../../common/decorators/to-lowercase.js';
 import { SignupDocument } from '../../firebase/models/signup.model.js';
 
 export class LookupInteractionDto
   implements Pick<SignupDocument, 'character' | 'world'>
 {
   @IsString()
-  @ToCasing(capitalCase)
+  @ToLowercase()
   character: string;
 
   @IsString()
   @IsOptional()
-  @ToCasing(capitalCase)
+  @ToLowercase()
   world: string;
 }
