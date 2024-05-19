0c98433c6d9af9f52b78c7e00541bed9d6994929
Steven Silvestri <steven.m.silvestri@gmail.com>
2024-05-19 08:35:46 -0600

    feat(turbo-prog): add turbo prog command

---
+++ src/commands/turboprog/turboprog.consts.ts
@@ -0,0 +1,13 @@
+export const TURBO_PROG_SUBMISSION_APPROVED = 'Turbo Prog submission approved!';
+
+export const TURBO_PROG_INACTIVE =
+  'Turbo Prog is not currently active. Please keep an eye out for any announcements.';
+
+export const TURBO_PROG_SIGNUP_INVALID =
+  'Only currently approved signups that are Clear Ready/Late Prog are eligible for Turbo Prog';
+
+export const TURBO_PROG_NO_SIGNUP_FOUND =
+  'No signup was found for this character. Only currently approved signups are eligible for Turbo Prog. Please use the **/signup** command to signup and then once approved you will be eligible for turbo prog.';
+
+export const TURBO_PROG_MISSING_SIGNUPS_SHEETS =
+  'The bot is missing a required configuration option for tubro prog to function. This issue has been reported and staff has been notified. Please try again later';
--- src/sheets/sheets.config.ts
+++ src/sheets/sheets.config.ts
@@ -6,6 +6,7 @@ export interface SheetsConfig {
   GOOGLE_UNIVERSE_DOMAIN: string;
   SHEET_EARLY_PROG_NAME: string;
   SHEET_PROG_NAME: string;
+  TURBO_PROG_SHEET_NAME: string;
 }
 
 const schema = Joi.object({
@@ -16,6 +17,7 @@ const schema = Joi.object({
   GOOGLE_UNIVERSE_DOMAIN: Joi.string().default('googleapis.com'),
   SHEET_EARLY_PROG_NAME: Joi.string().default('EARLY PROG (Season 4)'),
   SHEET_PROG_NAME: Joi.string().default('MID/LATE PROG (Season 4)'),
+  TURBO_PROG_SHEET_NAME: Joi.string().default('Bot'),
 });
 
 export const sheetsConfig = registerAs<SheetsConfig>('sheets', () => {
