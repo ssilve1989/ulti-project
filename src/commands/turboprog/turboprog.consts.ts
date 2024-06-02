f5f4269cf6bc05b976cd65b600ac57d8878b9842
Steve <steven.m.silvestri@gmail.com>
2024-06-02 14:34:33 -0600

    refactor: update turbo prog qualifiers (#329)

--- src/commands/turboprog/turboprog.consts.ts
+++ src/commands/turboprog/turboprog.consts.ts
@@ -4,7 +4,7 @@ export const TURBO_PROG_INACTIVE =
   'Turbo Prog signups are **closed**!. Please keep an eye out for any announcements.';
 
 export const TURBO_PROG_SIGNUP_INVALID =
-  'Only currently approved signups that are Clear Ready/Late Prog are eligible for Turbo Prog';
+  'Only currently approved signups that are Clear Ready are eligible for the Final Push';
 
 export const TURBO_PROG_NO_SIGNUP_FOUND =
   'No signup was found for this character. Only currently approved signups are eligible for Turbo Prog. Please use the **/signup** command to signup and then once approved you will be eligible for turbo prog.';
