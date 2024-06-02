6dc082737666c1db74f7fee0547c6252de135aa2
Steve <steven.m.silvestri@gmail.com>
2024-06-02 15:13:17 -0600

    feat: removes given roles on signup being marked cleared (#330)

---
+++ src/commands/signup/handlers/remove-roles.command-handler.ts
@@ -0,0 +1,40 @@
+import { Logger } from '@nestjs/common';
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import * as Sentry from '@sentry/node';
+import { DiscordService } from '../../../discord/discord.service.js';
+import { RemoveRolesCommand } from '../signup.commands.js';
+
+@CommandHandler(RemoveRolesCommand)
+export class RemoveRolesCommandHandler
+  implements ICommandHandler<RemoveRolesCommand>
+{
+  private readonly logger = new Logger(RemoveRolesCommandHandler.name);
+
+  constructor(public readonly discordService: DiscordService) {}
+
+  async execute({ encounter, userId, guildId, settings }: RemoveRolesCommand) {
+    const scope = Sentry.getCurrentScope();
+
+    try {
+      const member = await this.discordService.getGuildMember({
+        guildId,
+        memberId: userId,
+      });
+
+      const roles = [
+        settings.clearRoles?.[encounter],
+        settings.progRoles?.[encounter],
+      ].filter(Boolean) as string[];
+
+      if (member && roles.length > 0) {
+        await member.roles.remove(roles);
+        this.logger.log(
+          `Removed roles ${roles.join(', ')} from ${member.user.username}`,
+        );
+      }
+    } catch (error) {
+      scope.setExtras({ encounter, userId });
+      scope.captureException(error);
+    }
+  }
+}
