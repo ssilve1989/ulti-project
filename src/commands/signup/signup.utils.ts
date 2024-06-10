26be2c11e8a80f31e4b7a43b456b5ad7b882b768
Steven Silvestri <steven.m.silvestri@gmail.com>
2024-06-10 08:13:04 -0600

    refactor: migrate sagas to single side-effect file

--- src/commands/signup/signup.utils.ts
+++ src/commands/signup/signup.utils.ts
@@ -1,4 +1,5 @@
 import {
+  PartyStatus,
   SignupDocument,
   SignupStatus,
 } from '../../firebase/models/signup.model.js';
@@ -8,5 +9,14 @@ export function shouldDeleteReviewMessageForSignup({ status }: SignupDocument) {
   // may want to be kept as a reference for why a decision was made earlier
   return (
     status === SignupStatus.PENDING || status === SignupStatus.UPDATE_PENDING
+  );
+}
+
+export function hasClearedStatus({
+  partyType,
+  partyStatus,
+}: Pick<SignupDocument, 'partyStatus' | 'partyType'>) {
+  return (
+    partyType === PartyStatus.Cleared || partyStatus === PartyStatus.Cleared
   );
 }
