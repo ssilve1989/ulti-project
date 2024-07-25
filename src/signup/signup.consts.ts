c1c4a5a1018aeedb4357a412714a831cd6895584
Steve <steven.m.silvestri@gmail.com>
2024-07-25 15:29:19 -0600

    refactor(signup): cleanup depth complexity (#400)

--- src/signup/signup.consts.ts
+++ src/signup/signup.consts.ts
@@ -29,7 +29,7 @@ You can reach out to a coordinator to discuss any issues.
     'An error occurred while processing your response. The signup may not have been added to the Google Sheet, please verify it or add it manually',
   SIGNUP_NOT_FOUND_FOR_REACTION:
     'No signup was found in the database to correspond to this reaction. Check if you are reacting to a message that has already been handled, like a Cleared post. In those cases the document would be removed. If not please report this error.',
-};
+} as const;
 
 export const SIGNUP_REVIEW_REACTIONS: Record<SignupStatusValues, string> = {
   // biome-ignore lint/style/useNamingConvention: using literal enum values as keys
