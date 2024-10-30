480a10efefd60911bf626323bad3a3971de8b489
Steven Silvestri <steven.m.silvestri@gmail.com>
2024-10-30 07:18:13 -0600

    fix: fix coercion of env values

--- src/sheets/sheets.config.ts
+++ src/sheets/sheets.config.ts
@@ -5,7 +5,7 @@ const schema = z.object({
   // https://github.com/googleapis/google-api-nodejs-client/issues/3187
   // http2 + timeout maybe doesn't work well? We seem to be getting a lot more hanging requests
   // since this was turned on.
-  GOOGLE_APIS_HTTP2: z.boolean().default(false),
+  GOOGLE_APIS_HTTP2: z.coerce.boolean().default(false),
   GOOGLE_UNIVERSE_DOMAIN: z.string().default('googleapis.com'),
   TURBO_PROG_SHEET_NAME: z.string().default('Bot'),
 });
