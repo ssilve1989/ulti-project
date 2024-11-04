07e222c7b729e38fbab37c651fc0ff17c4c45ed5
Steven Silvestri <steven.m.silvestri@gmail.com>
2024-11-04 06:41:38 -0700

    fix: adds transform helper to createFields to assist wit null transformations

--- src/common/embed-helpers.ts
+++ src/common/embed-helpers.ts
@@ -1,7 +1,16 @@
 import type { APIEmbedField } from 'discord.js';
 
-type MaybeField = Omit<APIEmbedField, 'value'> & { value?: string | null };
+type WithTransform<T> = T & { transform?: (value: string) => string };
+
+type MaybeField = Omit<APIEmbedField, 'value'> &
+  WithTransform<{
+    value?: string | null;
+  }>;
 
 export function createFields(fields: MaybeField[]): APIEmbedField[] {
-  return fields.filter((field): field is APIEmbedField => !!field.value);
+  return fields
+    .filter((field): field is WithTransform<APIEmbedField> => !!field.value)
+    .map(({ transform, ...field }) =>
+      transform ? { ...field, value: transform(field.value) } : field,
+    );
 }
