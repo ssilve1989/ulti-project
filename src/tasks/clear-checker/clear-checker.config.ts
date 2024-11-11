ab409c35ec139131ed9eea40521b77327275c7a2
Steven Silvestri <steven.m.silvestri@gmail.com>
2024-11-11 13:54:54 -0700

    refactor(tasks): add off mode for clear-checker

--- src/tasks/clear-checker/clear-checker.config.ts
+++ src/tasks/clear-checker/clear-checker.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 import { z } from 'zod';
 
 const schema = z.object({
-  CLEAR_CHECKER_MODE: z.enum(['report', 'execute']).default('report'),
+  CLEAR_CHECKER_MODE: z.enum(['report', 'execute', 'off']).default('off'),
   CLEAR_CHECKER_CONCURRENCY: z.number().default(5),
 });
 
