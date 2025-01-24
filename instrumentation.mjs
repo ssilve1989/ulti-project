2d94cf65288fc3e7a198d6b46160eaa7365c58f1
Steven Silvestri <steven.m.silvestri@gmail.com>
2025-01-24 15:29:21 -0700

    build(bun): migrate to Bun runtime
    replace lockfile with bun.lockb
    
    chore: replace sentry with bun version

--- instrumentation.mjs
+++ instrumentation.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import * as Sentry from '@sentry/nestjs';
-import { nodeProfilingIntegration } from '@sentry/profiling-node';
 import pkg from './package.json' with { type: 'json' };
 
 const { NODE_ENV = "development", SENTRY_DEBUG, SENTRY_DSN } = process.env;
@@ -9,7 +8,6 @@ Sentry.init({
   debug: !!SENTRY_DEBUG,
   dsn: SENTRY_DSN,
   environment: NODE_ENV,
-  integrations: [nodeProfilingIntegration()],
   profilesSampleRate: 1.0,
   release: `ulti-project-bot@${pkg.version}`,
   tracesSampleRate: 1.0,
