efb3eba9aced795b9ed02927d689555ef0ab3f68
Steve <steven.m.silvestri@gmail.com>
2025-01-28 09:55:57 -0700

    refactor(remove-signup): update remove signup message (#646)

--- src/slash-commands/signup/subcommands/remove-signup/remove-signup.consts.ts
+++ src/slash-commands/signup/subcommands/remove-signup/remove-signup.consts.ts
@@ -7,4 +7,4 @@ export const REMOVAL_NO_SHEET_ENTRY =
   'No entry found in the spreadsheet. It is possible a coordinator has already removed your signup in which case you are all set! Please also verify that there are no typos in your character name or world';
 
 export const REMOVAL_NO_DB_ENTRY =
-  'No signup found. Please check the following fields for typos. Additionally this command will only work for signups that have gone through the `/signup` command. If you have signed up prior to the bots release please reach out to a coordinator to be removed from the sheet.';
+  'No signup found. Please check the following fields for typos. It is possible a coordinator or automated process has already removed your signup in which case you are all set!';
