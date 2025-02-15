34a45c9bfcee782a13379f152a43215befc2b81f
Steve <steven.m.silvestri@gmail.com>
2025-02-15 12:50:07 -0700

    feat(signups): confirm messages now mention user for searchability (#676)

--- src/common/components/fields.ts
+++ src/common/components/fields.ts
@@ -1,4 +1,4 @@
-import type { APIEmbedField } from 'discord.js';
+import { type APIEmbedField, userMention } from 'discord.js';
 import { titleCase } from 'title-case';
 import {
   Encounter,
@@ -6,13 +6,18 @@ import {
 } from '../../encounters/encounters.consts.js';
 
 export const characterField = (
-  value: string,
-  name = 'Character',
-): APIEmbedField => ({
-  name,
-  value: titleCase(value),
-  inline: true,
-});
+  character: string,
+  { name = 'Character', memberId }: { name?: string; memberId?: string } = {},
+): APIEmbedField => {
+  const value = memberId
+    ? `${titleCase(character)} (${userMention(memberId)})`
+    : titleCase(character);
+  return {
+    name,
+    value,
+    inline: true,
+  };
+};
 
 export const worldField = (value: string, name = 'World'): APIEmbedField => ({
   name,
