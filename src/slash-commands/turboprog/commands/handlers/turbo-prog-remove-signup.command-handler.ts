71c485859a14ce86dc5349f0bf2b9da78b721a13
Steve <steven.m.silvestri@gmail.com>
2025-03-09 09:44:21 -0600

    refactor(sheets): update service implementation (#712)
    Refactored sheets service implementation to improve performance and
    readability. Updated related handlers to use the new API.

--- src/slash-commands/turboprog/commands/handlers/turbo-prog-remove-signup.command-handler.ts
+++ src/slash-commands/turboprog/commands/handlers/turbo-prog-remove-signup.command-handler.ts
@@ -2,7 +2,7 @@ import { CommandHandler, type ICommandHandler } from '@nestjs/cqrs';
 import * as Sentry from '@sentry/node';
 import { SettingsCollection } from '../../../../firebase/collections/settings-collection.js';
 import { SentryTraced } from '../../../../sentry/sentry-traced.decorator.js';
-import { TurboProgSheetsService } from '../../../../sheets/turbo-prog-sheets/turbo-prog-sheets.service.js';
+import { SheetsService } from '../../../../sheets/sheets.service.js';
 import { TurboProgRemoveSignupCommand } from '../turbo-prog.commands.js';
 
 @CommandHandler(TurboProgRemoveSignupCommand)
@@ -10,7 +10,7 @@ class TurboProgRemoveSignupHandler
   implements ICommandHandler<TurboProgRemoveSignupCommand>
 {
   constructor(
-    private readonly sheetsService: TurboProgSheetsService,
+    private readonly sheetsService: SheetsService,
     private readonly settingsCollection: SettingsCollection,
   ) {}
 
@@ -22,7 +22,7 @@ class TurboProgRemoveSignupHandler
 
     try {
       if (spreadsheetId) {
-        await this.sheetsService.removeSignup(entry, spreadsheetId);
+        await this.sheetsService.removeTurboProgEntry(entry, spreadsheetId);
       }
     } catch (error) {
       scope.setExtra('entry', entry);
