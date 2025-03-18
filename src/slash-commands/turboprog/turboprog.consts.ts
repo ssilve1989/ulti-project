046e565d8019c5cfd7b498a7c257629e6d13b583
Steven Silvestri <steven.m.silvestri@gmail.com>
2025-03-18 06:15:39 -0600

    feat(final-push): support permanent final-push command

--- src/slash-commands/turboprog/turboprog.consts.ts
+++ src/slash-commands/turboprog/turboprog.consts.ts
@@ -1,13 +1,13 @@
-export const TURBO_PROG_SUBMISSION_APPROVED = 'Turbo Prog submission approved!';
+export const TURBO_PROG_SUBMISSION_APPROVED = 'Submission Accepted!';
 
 export const TURBO_PROG_INACTIVE =
-  'Turbo Prog signups are **closed**!. Please keep an eye out for any announcements.';
+  'Event signups are **closed**!. Please keep an eye out for any announcements.';
 
 export const TURBO_PROG_SIGNUP_INVALID =
-  'Only currently approved signups that are Clear Ready are eligible for the Turbo Prog events';
+  'Only currently approved signups that are Clear Ready are eligible for the event!';
 
 export const TURBO_PROG_NO_SIGNUP_FOUND =
-  'No signup was found for this character. Only currently approved signups are eligible for Turbo Prog. Please use the **/signup** command to signup and then once approved you will be eligible for turbo prog.';
+  'No signup was found for this character. Only currently approved signups are eligible. Please use the **/signup** command to signup and then once approved you will be eligible.';
 
 export const TURBO_PROG_MISSING_SIGNUPS_SHEETS =
-  'The bot is missing a required configuration option for tubro prog to function. This issue has been reported and staff has been notified. Please try again later';
+  'The bot is missing a required configuration option for this event to function. This issue has been reported and staff have been notified. Please try again later';
