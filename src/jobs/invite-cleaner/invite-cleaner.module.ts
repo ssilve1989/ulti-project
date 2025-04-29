25a9600cb46a6f203bedd868df3d66c35e766038
Steven Silvestri <steven.m.silvestri@gmail.com>
2025-04-29 11:06:29 -0600

    fix(jobs): fix DI error on startup

--- src/jobs/invite-cleaner/invite-cleaner.module.ts
+++ src/jobs/invite-cleaner/invite-cleaner.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { DiscordModule } from '../../discord/discord.module.js';
 import { FirebaseModule } from '../../firebase/firebase.module.js';
+import { inviteCleanerConfig } from './invite-cleaner.config.js';
 import { InviteCleanerJob } from './invite-cleaner.job.js';
 
 @Module({
-  imports: [DiscordModule, FirebaseModule],
+  imports: [
+    DiscordModule,
+    FirebaseModule,
+    ConfigModule.forFeature(inviteCleanerConfig),
+  ],
   providers: [InviteCleanerJob],
 })
 export class InviteCleanerModule {}
