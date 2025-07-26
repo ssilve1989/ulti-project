1602155f42c6299e8f99506cf5b57e65f60c05a4
Steven Silvestri <steven.m.silvestri@gmail.com>
2025-07-26 11:40:53 -0600

    refactor: adapt lookup error handling

--- src/slash-commands/lookup/lookup.module.ts
+++ src/slash-commands/lookup/lookup.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { ErrorModule } from '../../error/error.module.js';
 import { FirebaseModule } from '../../firebase/firebase.module.js';
 import { LookupCommandHandler } from './lookup.command-handler.js';
 
 @Module({
-  imports: [CqrsModule, FirebaseModule],
+  imports: [CqrsModule, ErrorModule, FirebaseModule],
   providers: [LookupCommandHandler],
 })
 class LookupModule {}
