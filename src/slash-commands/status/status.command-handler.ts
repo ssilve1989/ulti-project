3c9a5ff9ba4d4028f0547e34175fcc3790d14d58
Steven Silvestri <steven.m.silvestri@gmail.com>
2025-07-26 08:45:30 -0600

    refactor: adapt view-settings error handling

--- src/slash-commands/status/status.command-handler.ts
+++ src/slash-commands/status/status.command-handler.ts
@@ -1,37 +1,49 @@
-import { Logger } from '@nestjs/common';
 import { CommandHandler, type ICommandHandler } from '@nestjs/cqrs';
+import * as Sentry from '@sentry/nestjs';
 import { SentryTraced } from '@sentry/nestjs';
-import {
-  ChatInputCommandInteraction,
-  EmbedBuilder,
-  MessageFlags,
-} from 'discord.js';
+import { EmbedBuilder, MessageFlags } from 'discord.js';
 import { EncounterFriendlyDescription } from '../../encounters/encounters.consts.js';
+import { ErrorService } from '../../error/error.service.js';
 import {
   type SignupDocument,
   SignupStatus,
 } from '../../firebase/models/signup.model.js';
-import { sentryReport } from '../../sentry/sentry.consts.js';
 import { SIGNUP_REVIEW_REACTIONS } from '../signup/signup.consts.js';
 import { StatusCommand } from './status.command.js';
 import { StatusService } from './status.service.js';
 
 @CommandHandler(StatusCommand)
 class StatusCommandHandler implements ICommandHandler<StatusCommand> {
-  private readonly logger = new Logger(StatusCommandHandler.name);
-
-  constructor(private readonly service: StatusService) {}
+  constructor(
+    private readonly service: StatusService,
+    private readonly errorService: ErrorService,
+  ) {}
 
   @SentryTraced()
   async execute({ interaction }: StatusCommand) {
-    await interaction.deferReply({ flags: MessageFlags.Ephemeral });
-    try {
-      const signups = await this.service.getSignups(interaction.user.id);
-      const embed = this.createStatusEmbed(signups);
+    const scope = Sentry.getCurrentScope();
 
+    try {
+      await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+
+      const signups = await this.service.getSignups(interaction.user.id);
+
+      // Add context about the results
+      scope.setContext('status_results', {
+        signupCount: signups.length,
+        hasSignups: signups.length > 0,
+        encounters: signups.map((s) => s.encounter),
+      });
+
+      const embed = this.createStatusEmbed(signups);
       await interaction.editReply({ embeds: [embed] });
-    } catch (e: unknown) {
-      await this.handleError(e, interaction);
+    } catch (error) {
+      const errorEmbed = this.errorService.handleCommandError(
+        error,
+        interaction,
+        'Unable to retrieve your signup status. Please try again.',
+      );
+      await interaction.editReply({ embeds: [errorEmbed] });
     }
   }
 
@@ -71,19 +83,6 @@ class StatusCommandHandler implements ICommandHandler<StatusCommand> {
       );
     }
     return embed.addFields(fields);
-  }
-
-  private handleError(
-    error: unknown,
-    interaction: ChatInputCommandInteraction,
-  ) {
-    sentryReport(error);
-
-    this.logger.error(error);
-
-    return interaction.editReply({
-      content: 'Sorry an unexpected error has occurred',
-    });
   }
 }
 
