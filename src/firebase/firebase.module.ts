ad9ab18ba49dc83afcfc59c18a42a1a5bfcc5b23
Steven Silvestri <steven.m.silvestri@gmail.com>
2026-03-27 08:29:45 -0600

    refactor(firebase): extract createFirestore factory for reuse by CLI

--- src/firebase/firebase.module.ts
+++ src/firebase/firebase.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { type App, cert, initializeApp } from 'firebase-admin/app';
-import { getFirestore } from 'firebase-admin/firestore';
 import { appConfig } from '../config/app.js';
 import { firebaseConfig } from '../config/firebase.js';
 import { BlacklistCollection } from './collections/blacklist-collection.js';
@@ -8,32 +6,20 @@ import { EncountersCollection } from './collections/encounters-collection.js';
 import { JobCollection } from './collections/job/job.collection.js';
 import { SettingsCollection } from './collections/settings-collection.js';
 import { SignupCollection } from './collections/signup.collection.js';
-import { FIREBASE_APP, FIRESTORE } from './firebase.consts.js';
+import { createFirestore } from './create-firestore.js';
+import { FIRESTORE } from './firebase.consts.js';
 
 @Module({
   providers: [
     {
-      provide: FIREBASE_APP,
-      useFactory: () => {
-        return initializeApp({
-          credential: cert({
-            clientEmail: appConfig.GCP_ACCOUNT_EMAIL,
-            privateKey: appConfig.GCP_PRIVATE_KEY,
-            projectId: appConfig.GCP_PROJECT_ID,
-          }),
-        });
-      },
-    },
-    {
       provide: FIRESTORE,
-      inject: [FIREBASE_APP],
-      useFactory: (app: App) => {
-        const firestore = firebaseConfig.FIRESTORE_DATABASE_ID
-          ? getFirestore(app, firebaseConfig.FIRESTORE_DATABASE_ID)
-          : getFirestore(app);
-        firestore.settings({ ignoreUndefinedProperties: true });
-        return firestore;
-      },
+      useFactory: () =>
+        createFirestore({
+          clientEmail: appConfig.GCP_ACCOUNT_EMAIL,
+          privateKey: appConfig.GCP_PRIVATE_KEY,
+          projectId: appConfig.GCP_PROJECT_ID,
+          databaseId: firebaseConfig.FIRESTORE_DATABASE_ID,
+        }),
     },
     SignupCollection,
     SettingsCollection,
