2dd60d01c1df899846cfd02362e0cb3f486941be
Steven Silvestri <steven.m.silvestri@gmail.com>
2026-08-09 17:44:35 -0600

    test: exclude worktree dirs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
 import swc from 'unplugin-swc';
-import { defineConfig } from 'vitest/config';
+import { defaultExclude, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    exclude: [...defaultExclude, '.worktrees/**'],
     chaiConfig: {
       truncateThreshold: 80,
     },
