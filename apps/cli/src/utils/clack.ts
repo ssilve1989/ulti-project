696d598e6bbc3d79e307faa07fc54756373f92fc
Steve <steven.m.silvestri@gmail.com>
2026-09-22 07:26:55 -0600

    chore(deps): update dependency @clack/prompts to ^1.8.1 (#1528)
    Co-authored-by: Renovate <renovate@whitesourcesoftware.com>

--- apps/cli/src/utils/clack.ts
+++ apps/cli/src/utils/clack.ts
@@ -1,10 +1,10 @@
 import * as clack from '@clack/prompts';
 
-function isCancel(value: unknown): value is symbol {
+function isCancel(value: unknown): value is typeof clack.CANCEL_SYMBOL {
   return clack.isCancel(value);
 }
 
-export function cancelIfCancel<T>(value: T | symbol): T {
+export function cancelIfCancel<T>(value: T | typeof clack.CANCEL_SYMBOL): T {
   if (isCancel(value)) {
     clack.cancel('Operation cancelled.');
     process.exit(0);
